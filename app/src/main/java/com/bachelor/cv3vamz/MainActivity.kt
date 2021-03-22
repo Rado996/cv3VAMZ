575a4226a839c1e19f7c0041e767a58f8c1ba49b
Radovan Filický <rado.filickyp@gmail.com>
2021-03-22 09:15:54 +0100

    cv4 init

---
+++ app/src/main/java/com/bachelor/cv3vamz/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.bachelor.cv3vamz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.util.*
+import kotlin.random.Random.Default.nextInt
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollDice: Button = findViewById(R.id.button)
+        rollDice.setOnClickListener {
+            val textView: TextView = findViewById(R.id.textview1)
+            textView.text = (1..6).random().toString()
+        }
+
+    }
+}
+
+
+
